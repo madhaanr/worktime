df8267c599e241dcff6ea50c234464b8f8785545
Marko Haanranta <marko.haanranta@helsinki.fi>
2014-03-17 10:01:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Worktime::Application.config.secret_key_base = '0ab1982334849a1d37263039594c51b3c7166da3cbe97e4054952641c0f10569023c5b5f9263de991ec74b2bd565b2995392e39710cb89745185f0ca36c78176'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Worktime::Application.config.session_store :cookie_store, key: '_worktime_session'
